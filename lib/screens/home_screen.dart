2be21df767c81736ed42329a7e0bb33484da413a
Anar <anrwlsn@gmail.com>
2021-04-30 02:57:52 +0400

    home screen UI improve

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 import 'package:spotify_neumorphic/models/models.dart';
-import 'package:spotify_neumorphic/widgets/home_screen_welcome_tile.dart';
-import 'package:spotify_neumorphic/widgets/recently_played_list_tile.dart';
+import 'package:spotify_neumorphic/widgets/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -16,7 +16,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Welcome> _firstCards = [];
-  List<RecentlyPlayed> _recentCards = [];
+  List<Song> _recentCards = [];
+  List<Song> _playlistsFlow = [];
 
   @override
   void initState() {
@@ -26,47 +27,104 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final leftPadding = 15.0;
+
     return Scaffold(
       body: CustomScrollView(
         physics: BouncingScrollPhysics(),
         slivers: [
           SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: Text(
-                      'Good Morning',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 40,
+                  right: 15,
+                  bottom: 5,
+                ),
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  child: Neumorphic(
+                    style: NeumorphicStyle(
+                        depth: 4,
+                        intensity: .4,
+                        surfaceIntensity: .1,
+                        shape: NeumorphicShape.concave,
+                        shadowDarkColor: Colors.black,
+                        boxShape: NeumorphicBoxShape.circle(),
+                        color: Theme.of(context).scaffoldBackgroundColor),
+                    child: Neumorphic(
+                      style: NeumorphicStyle(
+                          depth: -1,
+                          intensity: .1,
+                          shadowDarkColor: Colors.black,
+                          boxShape: NeumorphicBoxShape.circle(),
+                          color: Theme.of(context).scaffoldBackgroundColor),
+                      child: Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Container(
+                          child: NeumorphicButton(
+                            padding: const EdgeInsets.only(right: .05),
+                            duration: const Duration(milliseconds: 100),
+                            curve: Curves.easeIn,
+                            onPressed: (() {}),
+                            style: NeumorphicStyle(
+                                depth: 6,
+                                intensity: .2,
+                                surfaceIntensity: .4,
+                                shadowDarkColor: Colors.black,
+                                shape: NeumorphicShape.convex,
+                                boxShape: NeumorphicBoxShape.circle(),
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor),
+                            child: Icon(
+                              Icons.settings_outlined,
+                              color: Colors.white.withOpacity(.5),
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                  GridView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 10.0,
-                      crossAxisSpacing: 5.0,
-                      childAspectRatio: (3 / 1),
-                    ),
-                    itemBuilder: (context, index) {
-                      return WelcomeTile(
-                        title: _firstCards[index].title,
-                        imagePath: _firstCards[index].imagePath,
-                        index: index,
-                      );
-                    },
-                    itemCount: _firstCards.length,
-                  ),
-                ],
+                ),
               ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: leftPadding),
+                  child: Text(
+                    'Good Morning',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10.0,
+                    crossAxisSpacing: 5.0,
+                    childAspectRatio: (3 / 1),
+                  ),
+                  itemBuilder: (context, index) {
+                    return WelcomeTile(
+                      title: _firstCards[index].title,
+                      imagePath: _firstCards[index].imagePath,
+                      index: index,
+                    );
+                  },
+                  itemCount: _firstCards.length,
+                ),
+              ],
             ),
           ),
           SliverToBoxAdapter(
@@ -75,19 +133,19 @@ class _HomeScreenState extends State<HomeScreen> {
               children: <Widget>[
                 SizedBox(height: 30),
                 Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
+                  padding: EdgeInsets.only(left: leftPadding),
                   child: Text(
                     'Recently Played',
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 23,
+                      fontSize: 22,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 SizedBox(height: 10),
                 Container(
-                  height: 250,
+                  height: 230,
                   child: ListView.builder(
                     physics: BouncingScrollPhysics(),
                     scrollDirection: Axis.horizontal,
@@ -106,6 +164,42 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
           ),
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.only(left: leftPadding),
+                  child: Text(
+                    'Playlists Flow',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 220,
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _playlistsFlow.length,
+                    itemBuilder: (context, index) {
+                      return PlaylistsFlowListTile(
+                        name: _playlistsFlow[index].name,
+                        artist: _playlistsFlow[index].artist,
+                        imagePath: _playlistsFlow[index].imagePath,
+                        index: index,
+                        last: _playlistsFlow.length - 1,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -115,18 +209,24 @@ class _HomeScreenState extends State<HomeScreen> {
     final welcomeJson = await rootBundle.loadString('assets/json/welcome.json');
     final recentsJson =
         await rootBundle.loadString('assets/json/recently_played.json');
+    final playlistsJson =
+        await rootBundle.loadString('assets/json/playlists_flow.json');
     final welcomeResponse = json.decode(welcomeJson);
     final recentsResponse = json.decode(recentsJson);
+    final playlistsResponse = json.decode(playlistsJson);
     final welcomeData = welcomeResponse['cards'] as List;
     final recentsData = recentsResponse['recently_played'] as List;
+    final playlistsData = playlistsResponse['playlists_flow'] as List;
     final firstCards =
         welcomeData.map<Welcome>((json) => Welcome.fromJson(json)).toList();
-    final recentsCards = recentsData
-        .map<RecentlyPlayed>((json) => RecentlyPlayed.fromJson(json))
-        .toList();
+    final recentsCards =
+        recentsData.map<Song>((json) => Song.fromJson(json)).toList();
+    final playlistsCards =
+        playlistsData.map<Song>((json) => Song.fromJson(json)).toList();
     setState(() {
       _firstCards = firstCards;
       _recentCards = recentsCards;
+      _playlistsFlow = playlistsCards;
     });
   }
 }
